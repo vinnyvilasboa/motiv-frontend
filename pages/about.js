9100420f49b12e7ff2b1cc933c546643ad98d78c
vinnyvilasboa <vinnycesca@gmail.com>
2022-06-21 13:57:51 -0400

    new page commits

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div>
+            <div className="title">
+                <h1>About Motiv</h1>
+            </div>
+
+        </div>
+    )
+}
---
+++ pages/signup.js
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div>
+            <div className="title">
+                <h1>Sign Up For Motiv</h1>
+            </div>
+
+        </div>
+    )
+}
