3735f3e8c969ea2933c4dd982123761e90011351
Andi Andreas <nexuister@gmail.com>
2022-06-26 17:55:01 -0400

    Update navbar to match mockup

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,43 +1,56 @@
-
-import styled from 'styled-components'
-import Link from 'next/link'
+import styled from "styled-components";
+import Link from "next/link";
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/"> 
-                <img src="https://i.postimg.cc/8Pq1PHLf/Motiv-Logo-png-removebg-preview-1.png" alt="motiv-logo" />
+                <a className="navbar-brand" href="/">
+                    <img
+                        src="https://i.postimg.cc/8Pq1PHLf/Motiv-Logo-png-removebg-preview-1.png"
+                        alt="motiv-logo"
+                    />
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarColor03"
+                    aria-controls="navbarColor03"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarColor03">
-                    <ul className="navbar-nav me-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home
+                    <ul className="navbar-nav ms-auto" style={{ gap: 68 }}>
+                        <li className="nav-active">
+                            <a className="nav-link" href="#">
+                                Home
                                 <span className="visually-hidden">(current)</span>
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/about">About Us</a>
+                            <a className="nav-link" href="/about">
+                                About Us
+                            </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/login">Login</a>
+                            <a className="nav-link" href="/login">
+                                Log in
+                            </a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/signup">Sign Up</a>
+                        <li className="nav-item" style={{ marginRight: 148 }}>
+                            <a className="btn btn-outline-dark" href="/signup">
+                                Sign Up
+                            </a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-    )
+    );
+};
 
-}
-
-export default Navbar
-
-
-
+export default Navbar;
