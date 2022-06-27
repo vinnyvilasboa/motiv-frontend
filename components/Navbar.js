ec3ddcaa01b5ce6130e9f3ae46af9dd6d5c4f145
vinnyvilasboa <vinnycesca@gmail.com>
2022-06-27 11:39:54 -0400

    updated Link from nav, minor tweaks

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,24 @@
 import styled from "styled-components";
 import Link from "next/link";
+// import { Link } from 'react-bootstrap'
 
+// const NavLinks = styled.nav`
+//     color: #263069;
+// 	font-size: 1.2rem;
+// 	font-weight: 700;
+    
+// `   
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">
+           {/* <NavLinks></NavLinks> */}
+                <Link className="navbar-brand" href="#">
                     <img
                         src="https://i.postimg.cc/8Pq1PHLf/Motiv-Logo-png-removebg-preview-1.png"
                         alt="motiv-logo"
                     />
-                </a>
+                </Link>
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -22,33 +30,51 @@ const Navbar = () => {
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
+                
                 <div className="collapse navbar-collapse" id="navbarColor03">
                     <ul className="navbar-nav ms-auto" style={{ gap: 68 }}>
                         <li className="nav-active">
-                        
-                            <a className="nav-link" href="/">
-                                Home
-                                <span className="visually-hidden">(current)</span>
-                            </a>
+
+                            <Link className="nav-link" href="/" style={{ textDecoration: 'none' }}><a style={{
+                                textDecoration: 'none',
+                                color: ' #263069',
+                                fontSize: '1.2rem',
+                                fontWeight: '700'
+                            }}>Home</a></Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/about">
-                                About Us
-                            </a>
+                            <Link className="nav-link" href="/about">
+                            <a style={{
+                                textDecoration: 'none',
+                                color: ' #263069',
+                                fontSize: '1.2rem',
+                                fontWeight: '700'
+                            }}>About Us</a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/login">
-                                Log in
-                            </a>
+                            <Link className="nav-link" href="/login">
+                            <a style={{
+                                textDecoration: 'none',
+                                color: ' #263069',
+                                fontSize: '1.2rem',
+                                fontWeight: '700'
+                            }}>Login</a>
+                            </Link>
                         </li>
                         <li className="nav-item" style={{ marginRight: 148 }}>
-                            <a className="btn btn-outline-dark" href="/signup">
-                                Sign Up
-                            </a>
+                            <Link className="btn btn-outline-dark" href="/signup">
+                            <a style={{
+                                textDecoration: 'none',
+                                color: ' #263069',
+                                fontSize: '1.2rem',
+                                fontWeight: '700'
+                            }}>Sign Up</a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
+           
             </div>
         </nav>
     );
--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
             <Script
                 src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
                 integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-                crossOrigin="anonymous"
+                crossOrigin="anonymous" async
             />
             
             <Navbar/>
