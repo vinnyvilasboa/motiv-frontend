462f50f4e19a6e8ac16ecb2a02612d97a1ac07b8
vinnyvilasboa <vinnycesca@gmail.com>
2022-06-27 11:43:56 -0400

    deploy error fix

--- pages/index.js
+++ pages/index.js
@@ -38,11 +38,11 @@ export default function Home() {
 
                 <script
                     src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-                    crossOrigin></script>
+                    async></script>
 
                 <script
                     src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-                    crossOrigin></script>
+                    async></script>
             </footer>
         </div>
 
