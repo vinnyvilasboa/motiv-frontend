6ee7ea16369032b01db0e0fd1312b3d5f23a4d2e
Andi Andreas <nexuister@gmail.com>
2022-07-05 21:33:02 -0400

    Refactored BottomComponent into index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
+import { Container } from "react-bootstrap";
 import HeroComponent from "../components/HeroComponent";
 import NetworkComponent from "../components/NetworkComponent";
 import CareerComponent from "../components/CareerComponent";
 import PlayerProfile from "../components/PlayerProfile";
 import MessageComponent from "../components/MessageComponent";
-import BottomComponent from "../components/BottomComponent";
 import Footer from "../components/Footer";
+import styles from "../styles/index.module.css";
 
 export default function Home() {
 	return (
@@ -18,8 +19,18 @@ export default function Home() {
 				<NetworkComponent />
 				<CareerComponent />
 				<PlayerProfile />
-                <MessageComponent />
-				<BottomComponent />
+				<MessageComponent />
+				<Container fluid className={styles.footerHeader + " text-center"}>
+					<h1 className={styles.footerHeaderTitle}>
+						Gain The Competitive Edge You Deserve
+					</h1>
+					<p className={styles.footerHeaderSubtitle}>
+						We aim to democratize how student athletes are hired by creating a
+						competitive landscape that allows for equal opportunities. Student
+						athletes have a unique set of skill sets that are highly desirable
+						to employers and we seek to showcase that.
+					</p>
+				</Container>
 			</main>
 			<Footer />
 		</div>
