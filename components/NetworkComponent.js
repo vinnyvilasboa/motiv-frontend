4b8feed6fa89699b63e538e0c9e3b281769031d7
vinnyvilasboa <vinnycesca@gmail.com>
2022-07-08 00:45:43 -0400

    network comp - mobile responsive

--- components/NetworkComponent.js
+++ components/NetworkComponent.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
-import { Container, Row, Col, Img } from 'react-bootstrap'
+import Figure from 'react-bootstrap/Figure'
+import FigureImage from 'react-bootstrap/FigureImage'
+import FigureCaption from 'react-bootstrap/FigureCaption'
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import styles from "../styles/network.module.css";
 import SignupIcon from '../public/assets/Motive/icons/SignupIcon.svg'
 import SharePlayer from '../public/assets/Motive/icons/SharePlayer.svg'
@@ -13,62 +16,57 @@ import MotivLogo from '../public/assets/Motive/icons/motivLogo.png'
 const NetworkComponent = () => {
     return (
         <>
-            <div className="network-bg">
+            <div className={styles.networkBG}>
 
                 <Container fluid className="text-center">
                     <Row>
-                        <h1 className={styles.sectionTitle}>Get Started and Grow your Network</h1>
+                        <h1 className={styles.sectionTitle} style={{fontSize:"2.5rem"}}>Get Started and Grow your Network</h1>
                     </Row>
                     <Row>
                         <p className="styles sectionText">
                             Motiv's Profile creation process is fast and easy
                         </p>
                     </Row>
-                    
+
 
                 </Container>
                 <Container fluid >
-                        <Row className={styles.iconsCenter}>
-                            <Col sm={4} className={styles.iconCol}>
-                                <Card className={styles.iconCard}>
+                    <Row className={styles.iconsCenter}>
+                        <Col sm={4} className={styles.iconCol}>
+                            
+                            <Image 
+                            src={require("../public/assets/Motive/icons/SignupIcon.svg").default.src}
+                            width={100}
+                            height={100}
+                            className={styles.icons}/>
+                             <h3 className={styles.iconText}>Sign Up <br/> With Your Univerisity Email</h3>
+                             <p className={styles.subtext} >lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipis</p>
+                        
+                            
+                        </Col>
+                        <Col sm={4} className={styles.iconCol}>
+                        <Image 
+                            src={require("../public/assets/Motive/icons/SharePlayer.svg").default.src}
+                            width={100}
+                            height={100}
+                            className={styles.icons}/>
+                              <h3 className={styles.iconText}>Share Your <br /> "Player Profile Card"</h3>
+                              <p className={styles.subtext} >lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipis</p>
 
-                                    <Card.Img variant="top" src={SignupIcon} />
-                                    <Card.Body>
-                                        <Card.Title>Sign Up With Your Univerisity Email</Card.Title>
-                                        <Card.Text>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        </Card.Text>
+                        </Col>
+                        <Col sm={4} className={styles.iconCol}>
+                        <Image 
+                            src={require("../public/assets/Motive/icons/ConnectNCAA.svg").default.src}
+                            width={100}
+                            height={100}
+                            className={styles.icons}/>
+                              <h3 className={styles.iconText}>Connect <br/> with Other NCAA athletes</h3>
+                              <p className={styles.subtext} >lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipis</p>
 
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                            <Col sm={4} className={styles.iconCol}>
-                            <Card className={styles.iconCard}>
-                                    <Card.Img variant="top" src={SharePlayer} />
-                                    <Card.Body>
-                                        <Card.Title>Share Your <br /> "Player Profile Card"</Card.Title>
-                                        <Card.Text>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        </Card.Text>
+                        </Col>
+                    </Row>
 
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                            <Col sm={4} className={styles.iconCol}>
-                            <Card Card className={styles.iconCard}>
-                                    <Card.Img variant="top" src={ConnectNCAA} />
-                                    <Card.Body>
-                                        <Card.Title>Connect with Other NCAA athletes</Card.Title>
-                                        <Card.Text>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        </Card.Text>
-
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-
-                        </Row>
-                    </Container>
+                </Container>
             </div>
         </>
 
